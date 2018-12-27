da7a38655d640abe93ef810d6b476b717bffd466
Patryk Bogdański <bogdanski.patryk1991@gmail.com>
2018-12-27 20:17:00 +0100

    Kamera jest w stałej pozycji, sledzi gracza

---
+++ Fit Into/Assets/Scripts/Player/CameraFollower.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour
+{
+    [SerializeField]
+    private int _distanceFromPlayer;
+    private MoveController _moveController;
+
+    private MoveController MoveController
+    {
+        get
+        {
+            if (null == _moveController)
+            {
+                _moveController = GameObject.FindObjectOfType<MoveController>();
+            }
+            return _moveController;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, MoveController.transform.position.z - _distanceFromPlayer);
+    }
+}
