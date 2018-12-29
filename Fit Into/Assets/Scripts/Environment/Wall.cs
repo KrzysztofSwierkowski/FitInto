93a7faf4f9aef09d667d50e5d50da266eb982ded
Patryk Bogdański <bogdanski.patryk1991@gmail.com>
2018-12-29 22:49:09 +0100

    Próba SelfDestruct

--- Fit Into/Assets/Scripts/Environment/Wall.cs
+++ Fit Into/Assets/Scripts/Environment/Wall.cs
@@ -7,8 +7,22 @@ public class Wall : MonoBehaviour
 {
     public Shape[] AcceptableShapes;
 
+    [SerializeField]
+    GameObject _OnDestroyObject;
+
     public bool AcceptShape(Shape shape)
     {
         return AcceptableShapes.Contains(shape);
+    }
+
+    private void OnDestroy()
+    {
+        if (_OnDestroyObject != null)
+        {
+            GameObject go = GameObject.Instantiate(_OnDestroyObject);
+            var destroy = go.AddComponent<SelfDestroy>();
+            destroy.Active(2f);
+            go.transform.position = this.transform.position;
+        }
     }
 }
